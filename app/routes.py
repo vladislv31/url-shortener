88632a38aaa72ce61948ae299c5ad7a8fd94f6ac
vladislv31 <vladislv.k31@gmail.com>
2020-09-12 03:07:30 +0300

    main page title and redirect if 404

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -13,3 +13,7 @@ def index():
         new_link = link
 
     return render_template('index.html', link=link, new_link=new_link)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
